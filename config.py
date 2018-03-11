d6bb335b95636ea2f9fc071b33a00d7617413907
Jun Luo <luojdev@gmail.com>
2018-03-11 12:20:18 +0800

    Convert tab to 4 space

--- config.py
+++ config.py
@@ -2,26 +2,26 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'a random key'
-	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a random key'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
-	@staticmethod
-	def init_app(app):
-		pass
+    @staticmethod
+    def init_app(app):
+        pass
 
 class DevelopmentConfig(Config):
-	DEBUG = True
+    DEBUG = True
 
 class TestingConfig(Config):
-	TESTING = True
+    TESTING = True
 
 class ProductionConfig(Config):
-	pass
+    pass
 
 config = {
-	'development': DevelopmentConfig,
-	'testing': TestingConfig,
-	'production': ProductionConfig,
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
 
-	'default': DevelopmentConfig
+    'default': DevelopmentConfig
 }
--- manage.py
+++ manage.py
@@ -7,4 +7,4 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
